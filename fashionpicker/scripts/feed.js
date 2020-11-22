a55ab44e62647f826681df501401db6e9fac8c20
b-andre-fct <73527308+b-andre-fct@users.noreply.github.com>
2020-11-22 18:22:08 +0000

    Script tweak

--- fashionpicker/scripts/feed.js
+++ fashionpicker/scripts/feed.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $.ajax({
-        url : "database/",
+        url : "database",
         success: function (data) {
             $(data).find("a").attr("href", function (i, val) {
-                if(val.match(/\.(jpe?g|png)$/) ) { 
+                if(val.match(/\.(jpe?g|png|JPG)$/) ) { 
                     $("body").append( "<br> <img src='" + val +"'>" );
                 } 
             });
