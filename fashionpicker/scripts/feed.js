70f16fc318146de40eefbb82b8177d56b46ef194
unknown <talm.oliveira@campus.fct.unl.pt>
2020-11-25 16:56:29 +0000

    added style for the feed page

--- fashionpicker/scripts/feed.js
+++ fashionpicker/scripts/feed.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     for(var counter = 1; counter <= 88; counter++) {
+
+        
         var picture = document.createElement("img");
 
         picture.className = "image-feed";
         picture.src = "./database/" + counter + ".jpg";
         picture.style.height = "200px";
         picture.style.width = "200px";
-        document.body.appendChild(picture);
+        $(".feed-designs").append(picture);
     }
 });
