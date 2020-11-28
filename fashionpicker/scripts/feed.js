5fa44e9477185f03262aeeb0e5dc4e36b8a60b30
Bea <b.andre@campus.fct.unl.pt>
2020-11-28 16:23:00 +0000

    Added usernames array

--- fashionpicker/scripts/feed.js
+++ fashionpicker/scripts/feed.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     for(var counter = 1; counter <= 88; counter++) {
-
         
         var picture = document.createElement("img");
 
@@ -19,3 +18,28 @@ function loadMyPage() {
 function loadSettings() {
     window.location = "./user-settings.html";
 }
+
+var users = [
+    "Cloth Collage", "Fantasy Dress", "Streetwear Sam", "Concept Fashion",
+    "Fantasy Dress", "Matt Wood", "Streetwear Sam", "Cottagecore",
+    "Crew Cuts", "Fantasy Dress", "Streetwear Sam", "Fantasy Dress",
+    "Cloth Collage", "Matt Wood", "Jewellery by Luna", "Jewellery by Luna",
+    "Jewellery by Luna", "Altered Reality", "Altered Reality", "Mady Makes",
+    "Mady Makes", "Ana Accessories", "Linda White", "Fantasy Dress",
+    "Midori", "Helen Williams", "Cloth Collage", "Creeps",
+    "Creeps", "Dance Overnight", "Dance Overnight", "Dress by Duarte",
+    "Amaya", "Cottagecore", "Dress by Duarte", "Ghost Buddy",
+    "Ghost Buddy", "Ghost Buddy", "Fantasy Dress", "Matt Wood",
+    "Dress by Duarte", "Matt Wood", "Sarah Ainsley", "Kozi Klothes",
+    "Mary B", "Mary B", "Cloth Collage", "Cloth Collage",
+    "Dress by Duarte", "Outlandish", "Outlandish", "Outlandish",
+    "Outlandish", "Matt Wood", "Isaac Clay", "Swell Swim",
+    "Kristin", "Ana Accessories", "Ana Accessories", "Matt Wood",
+    "Ivywear", "Pete does Pants", "Streetwear Sam", "Soft Pastel",
+    "Soft Pastel", "Kabayashi", "Kabayashi", "Kabayashi",
+    "Swell Swim", "Fantasy Dress", "Streetwear Sam", "Soft Pastel",
+    "Totem Brand", "Totem Brand", "Totem Brand", "Totem Brand",
+    "Totem Brand", "Totem Brand", "Solstice Wear", "Solstice Wear",
+    "Graphic Tees", "Graphic Tees", "Graphic Tees", "Dress by Duarte",
+    "Concept Fashion", "Cloth Collage", "Swell Swim" , "Concept Fashion"
+];
