b38f8ddc6cb9c049e2ea9bb0d6b643e154480106
unknown <talm.oliveira@campus.fct.unl.pt>
2020-11-29 17:27:53 +0000

    style for the user page

--- fashionpicker/scripts/user-area.js
+++ fashionpicker/scripts/user-area.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     $("#username").text(sessionStorage.getItem("username"));
     $("#bio").text(sessionStorage.getItem("bio"));
+
+    $(".my-created-designs p").addClass("active-profile-setting");
+
+    
+
 });
